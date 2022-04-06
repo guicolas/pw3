8b0fe9267ed7057eeb36a4aa302b56953a75acbb
guicolas <email@test.com>
2022-04-06 13:33:53 -0300

    Criacao do projeto

---
+++ src/test/java/br/com/seunome/sobrenome/detran/Pw3DetranApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seunome.sobrenome.detran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3DetranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
