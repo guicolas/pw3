4d66f60e9e52e88ba489dbda9813463422e07dae
guicolas <email@test.com>
2022-04-06 14:31:41 -0300

    Criacao da entidade Multa

---
+++ src/main/java/br/com/etechoracio/pw3_detran/entity/Multa.java
@@ -0,0 +1,17 @@
+package br.com.etechoracio.pw3_detran.entity;
+
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Column 
+public class Multa {
+int pessoa;
+string nome;
+string endereco;
+
+}
